11eff35bc96fadcf778dec0043cf58d484bb84c3
Kirsten Bachert <kb@buildmidwestern.com>
2024-04-08 09:15:06 -0600

    fix: hero and text banner images on alt page 1

--- kd-sanity-starter/src/components/NavBar1.tsx
+++ kd-sanity-starter/src/components/NavBar1.tsx
@@ -1,30 +1,33 @@
+import CityTechIcon from '@public/CityTechIcon.svg';
+import Image from 'next/image';
+
 const NavBar1 = () => {
   return (
     <div className='self-stretch h-[59px] p-2 bg-black justify-between items-center inline-flex'>
       <div className='px-2 justify-start items-center gap-2 flex'>
-        <div className='w-6 h-6 relative' />
+        <Image src={CityTechIcon} alt='CityTech Icon' />
         <div className="text-neutral-100 text-base font-normal font-['Fira Sans'] leading-normal">
           CityTech
         </div>
       </div>
       <div className='w-[334px] px-4 justify-start items-center gap-4 flex'>
-        <div className='w-[91px] self-stretch px-4 justify-start items-center gap-2 flex'>
+        <div className='w-[100px] self-stretch px-4 justify-start items-center gap-2 flex'>
           <div className="text-neutral-100 text-xs font-normal font-['Fira Sans'] leading-tight">
             Dashboard
           </div>
         </div>
-        <div className='w-[97px] self-stretch px-4 justify-start items-center gap-2 flex'>
+        <div className='w-[100px] self-stretch px-4 justify-start items-center gap-2 flex'>
           <div className="text-neutral-100 text-xs font-normal font-['Fira Sans'] leading-tight">
             Innovations
           </div>
         </div>
-        <div className='w-[82px] self-stretch px-4 justify-start items-center gap-2 flex'>
+        <div className='w-[100px] self-stretch  justify-start items-center gap-2 flex'>
           <div className="text-neutral-100 text-xs font-normal font-['Fira Sans'] leading-tight">
             Our Crew
           </div>
         </div>
       </div>
-      <div className='w-[116px] px-5 py-3 border border-neutral-100 justify-center items-center gap-1 flex'>
+      <div className='w-[121px] px-5 py-3 border border-neutral-100 justify-center items-center gap-1 flex'>
         <div className='px-1 justify-center items-center gap-2 flex'>
           <div className="text-center text-neutral-100 text-xs font-normal font-['Fira Sans'] leading-tight">
             Explore Now
--- kd-sanity-starter/src/components/TextBannerWithBackground1.tsx
+++ kd-sanity-starter/src/components/TextBannerWithBackground1.tsx
@@ -1,7 +1,10 @@
+import BackgroundImageStyles from '../styles/background.module.css';
+
 const TextBannerWithBackground1 = () => {
   return (
-    <div className='self-stretch px-4 py-[184px] justify-center items-center gap-12 inline-flex'>
-      {/* <img className='w-[1280px] h-[563px]' src='https://via.placeholder.com/1280x563' /> */}
+    <div
+      className={`${BackgroundImageStyles.textBanner1} self-stretch px-4 py-[184px] justify-center items-center gap-12 inline-flex`}
+    >
       <div className='w-[1280px] h-[563px] relative bg-black bg-opacity-20' />
       <div className='grow shrink basis-0 flex-col justify-start items-center gap-2 inline-flex'>
         <div className="self-stretch text-center text-neutral-50 text-7xl font-extrabold font-['Playfair Display'] leading-[77.76px]">

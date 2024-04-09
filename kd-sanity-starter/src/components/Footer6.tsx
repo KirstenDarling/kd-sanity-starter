9c890a15d1564c2b10486d821efec57d1946b382
Kirsten Bachert <kb@buildmidwestern.com>
2024-04-08 22:26:51 -0600

    fix: adjusted alt landing pages six and seven

--- kd-sanity-starter/src/components/Footer6.tsx
+++ kd-sanity-starter/src/components/Footer6.tsx
@@ -1,9 +1,17 @@
+import Image from 'next/image';
+import InstagramIcon from '../../public/InstagramLogo.svg';
+import LinkedInIcon from '../../public/LinkedinLogo.svg';
+import TwitterIcon from '../../public/TwitterLogo.svg';
+import HeartIcon from '../../public/heartIcon.svg';
+
 const Footer6 = () => {
   return (
     <div className='self-stretch px-8 py-12 bg-black justify-between items-center inline-flex'>
       <div className='grow shrink basis-0 h-[58px] justify-between items-center flex'>
         <div className='flex-col justify-center items-center gap-1 inline-flex'>
-          <div className='w-8 h-8 relative'></div>
+          <div className='w-8 h-8 relative'>
+            <Image src={HeartIcon} alt='' />
+          </div>
           <div className="text-neutral-100 text-sm font-normal font-['Alegreya'] leading-snug">
             ShadowBytes
           </div>
@@ -25,9 +33,15 @@ const Footer6 = () => {
           </div>
         </div>
         <div className='justify-start items-start gap-4 flex'>
-          <div className='w-6 h-6 relative'></div>
-          <div className='w-6 h-6 relative'></div>
-          <div className='w-6 h-6 relative'></div>
+          <div className='w-6 h-6 relative'>
+            <Image alt='' src={TwitterIcon} />
+          </div>
+          <div className='w-6 h-6 relative'>
+            <Image alt='' src={InstagramIcon} />
+          </div>
+          <div className='w-6 h-6 relative'>
+            <Image alt='' src={LinkedInIcon} />
+          </div>
         </div>
       </div>
     </div>
--- kd-sanity-starter/src/components/Newsletter7.tsx
+++ kd-sanity-starter/src/components/Newsletter7.tsx
@@ -1,8 +1,11 @@
+import BackgroundImageStyles from '../styles/background.module.css';
+
 const Newsletter7 = () => {
   return (
-    <div className='self-stretch h-[686px] px-4 py-[120px] flex-col justify-center items-center flex'>
-      {/* <Image className='w-[1280px] h-[686px]' src='https://via.placeholder.com/1280x686' /> */}
-      <div className='self-stretch h-[446px] p-8 bg-black rounded-2xl flex-col justify-center items-center gap-4 flex'>
+    <div
+      className={` ${BackgroundImageStyles.newsletterBackground} self-stretch h-[686px] px-4 py-[120px] flex-col justify-center items-center flex`}
+    >
+      <div className='self-stretch h-[446px] max-w-[480px] m-[auto] p-8 bg-black flex-col justify-center items-center gap-4 flex'>
         <div className='self-stretch h-[82px] flex-col justify-center items-center gap-2 flex'>
           <div className="self-stretch text-center text-neutral-100 text-4xl font-extrabold font-['Alegreya'] leading-[46.80px]">
             Join Today
@@ -11,21 +14,21 @@ const Newsletter7 = () => {
             Secure your spot!
           </div>
         </div>
-        <div className='self-stretch px-2 py-4 bg-gray-800 rounded border border-gray-600 justify-center items-center gap-3 inline-flex'>
-          <div className='w-6 h-6 relative' />
+        <div className='self-stretch px-2 py-4 bg-gray-800 border border-gray-600 justify-center items-center gap-3 inline-flex'>
+          <div className='w-6 h-6 relative'></div>
           <div className="text-center text-neutral-100 text-sm font-normal font-['Alegreya'] leading-snug">
             Connect with Google
           </div>
         </div>
         <div className='self-stretch justify-center items-center gap-6 inline-flex'>
-          <div className='w-[178.50px] h-px relative bg-gray-600' />
+          <div className='w-[178.50px] h-px relative bg-gray-600'></div>
           <div className="text-center text-zinc-300 text-xs font-normal font-['Alegreya'] leading-tight">
             or
           </div>
-          <div className='w-[178.50px] h-px relative bg-gray-600' />
+          <div className='w-[178.50px] h-px relative bg-gray-600'></div>
         </div>
         <div className='self-stretch h-[142px] flex-col justify-start items-center gap-2 flex'>
-          <div className='self-stretch h-20 rounded flex-col justify-center items-start gap-1 flex'>
+          <div className='self-stretch h-20 flex-col justify-center items-start gap-1 flex'>
             <div className="self-stretch text-neutral-100 text-sm font-normal font-['Alegreya'] leading-snug">
               Username or Email
             </div>
@@ -37,7 +40,7 @@ const Newsletter7 = () => {
               </div>
             </div>
           </div>
-          <div className='self-stretch px-6 py-4 bg-purple-500 rounded justify-center items-center gap-1 inline-flex'>
+          <div className='self-stretch px-6 py-4 bg-purple-500 justify-center items-center gap-1 inline-flex'>
             <div className='grow shrink basis-0 h-[22px] px-1 justify-center items-center gap-2 flex'>
               <div className="grow shrink basis-0 text-center text-white text-sm font-normal font-['Alegreya'] leading-snug">
                 Proceed with Email

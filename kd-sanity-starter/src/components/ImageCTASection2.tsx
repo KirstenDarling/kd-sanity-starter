c65f4dc24924536fa32942de5a799d4867523d24
Kirsten Bachert <kb@buildmidwestern.com>
2024-04-15 09:55:40 -0600

    fix: cta section on alt landing page 2

--- kd-sanity-starter/src/components/ImageCTASection2.tsx
+++ kd-sanity-starter/src/components/ImageCTASection2.tsx
@@ -1,28 +1,33 @@
-import Image from 'next/image';
-import ImageLummiFinal from '../../public/imageLummiFinal.png';
+import Image, { StaticImageData } from 'next/image';
 
-const ImageCTASection2 = () => {
+const ImageCTASection2 = ({
+  heading,
+  subheading,
+  CTAText,
+  image,
+}: {
+  heading?: string;
+  subheading?: string;
+  CTAText?: string;
+  image: StaticImageData;
+}) => {
   return (
     <div className='self-stretch bg-rose-500 justify-center items-start inline-flex'>
-      <Image
-        className='max-w-[640px] grow shrink basis-0 h-[803px] py-12'
-        src={ImageLummiFinal}
-        alt=''
-      />
+      <Image className='max-w-[640px] grow shrink basis-0 h-[803px] py-12' src={image} alt='' />
       <div className='grow shrink basis-0 h-[803px] px-[5%] py-20 justify-center items-center gap-2 flex'>
         <div className='grow shrink basis-0 bg-rose-500 rounded-[32px] flex-col justify-center items-start gap-8 inline-flex'>
           <div className='self-stretch h-[102px] flex-col justify-center items-start gap-4 flex'>
             <div className="self-stretch text-orange-200 text-[51px] font-bold font-['Syne'] leading-[61.20px]">
-              Let&apos;s Collaborate!
+              {heading}
             </div>
             <div className="self-stretch text-amber-300 text-lg font-normal font-['Work Sans'] leading-[25.20px]">
-              Ready to build something amazing together?
+              {subheading}
             </div>
           </div>
           <div className='max-w-[360px] self-stretch px-12 py-4 bg-fuchsia-700 rounded-lg justify-center items-center gap-1 inline-flex'>
             <div className='px-1 justify-center items-center gap-2 flex'>
               <div className="text-center text-orange-200 text-lg font-normal font-['Work Sans'] leading-[25.20px]">
-                Get in Touch
+                {CTAText}
               </div>
             </div>
           </div>

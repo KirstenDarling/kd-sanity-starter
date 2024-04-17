a9aa09b257c23d86a33a6e25e9880ae85dac2f1d
Kirsten Darling <54489152+KirstenDarling@users.noreply.github.com>
2024-04-17 10:28:50 -0600

    Combine alts 6and7 (#55)
    * fix: simplified cards used on alt landing page five
    
    * fix: mobile preview section
    
    * fix: folded mobile preview section 7 into mobile preview section 6 since they were the same
    
    * fix: condensed numbers accordion 6
    
    * fix: condensed newseltter on alt landing page seven
    
    * fix: added props to quote component on page 7

--- kd-sanity-starter/src/pages/alt-landing-page-seven/index.tsx
+++ kd-sanity-starter/src/pages/alt-landing-page-seven/index.tsx
@@ -1,15 +1,36 @@
 // import Image from 'next/image';
 import Footer7 from '@/components/Footer7';
 import Hero7 from '@/components/Hero7';
-import MobilePreviewSection7 from '@/components/MobilePreviewSection7';
+import MobilePreviewSection from '@/components/MobilePreviewSection';
 import NavBar7 from '@/components/NavBar7';
-import Newsletter7 from '@/components/Newsletter7';
-import Quote7 from '@/components/Quote7';
+import Newsletter from '@/components/Newsletter';
+import Quote from '@/components/Quote';
 import SubscriptionPlans from '@/components/SubscriptionPlans';
 import TextBannerWithoutBackground from '@/components/TextBannerWithoutBackground';
+import MobileMockupImage7 from '../../../public/MobileMockup7.png';
+import CatPic from '../../../public/catPic.png';
 import Media7Layout from '../../../public/mediaLayout7.png';
+import MessageIcon from '../../../public/messageIcon.svg';
 
 export default function Home() {
+  const featuredBullets = [
+    {
+      title: 'Real-time Processing',
+      subtitle: 'Ultra-fast computation and analysis.',
+      icon: MessageIcon,
+    },
+    {
+      title: 'Scalable Infrastructure',
+      subtitle: 'Adaptable to growing data needs.',
+      icon: MessageIcon,
+    },
+    {
+      title: 'Secure Cloud Storage',
+      subtitle: 'Protecting your data at all costs.',
+      icon: MessageIcon,
+    },
+  ];
+
   return (
     <div className='w-full h-[4818.97px] bg-white flex-col justify-start items-center inline-flex'>
       <NavBar7 />
@@ -24,10 +45,28 @@ export default function Home() {
         heading='Pioneering Data Horizons.'
         subheading='Data solutions reimagined'
       />
-      <MobilePreviewSection7 />
+      <MobilePreviewSection
+        title='Advanced Analytics'
+        subtitle='Leverage insights effectively.'
+        mobileImage={MobileMockupImage7}
+        bullets={featuredBullets}
+      />
       <SubscriptionPlans />
-      <Newsletter7 />
-      <Quote7 />
+      <Newsletter
+        title='Join Today'
+        subtitle='Secure your spot!'
+        buttonOptionOneText='Connect with Google'
+        inputLabel='Username or Email'
+        input='Create a Password'
+        buttonOptionTwoText='Proceed with Email'
+        legalText='Terms and privacy agreed upon'
+        image={2}
+      />
+      <Quote
+        quote='“DataWarp transformed our data strategy and execution.”'
+        author='Ava Smith, Data Scientist'
+        image={CatPic}
+      />
       <Footer7 />
     </div>
   );
--- kd-sanity-starter/src/pages/alt-landing-page-six/index.tsx
+++ kd-sanity-starter/src/pages/alt-landing-page-six/index.tsx
@@ -2,14 +2,52 @@
 
 import Footer6 from '@/components/Footer6';
 import Hero6 from '@/components/Hero6';
-import MobilePreviewSection6 from '@/components/MobilePreviewSection6';
+import MobilePreviewSection from '@/components/MobilePreviewSection';
 import NavBar6 from '@/components/NavBar6';
-import Newsletter6 from '@/components/Newsletter6';
-import NumberAccordion6 from '@/components/NumberAccordion6';
+import Newsletter from '@/components/Newsletter';
+import NumberAccordion from '@/components/NumberAccordion';
 import TextBannerAltStyleSplit from '@/components/TextBannerAltStyleSplit';
 import HeroImage6 from '../../../public/heroImage6.png';
+import MessageIcon from '../../../public/messageIcon.svg';
+import MobileMockupImage from '../../../public/mobileMockupImage.png';
 
 export default function Home() {
+  const featuredBullets = [
+    {
+      title: 'Advanced Algorithms',
+      subtitle: 'Decoding complexities, simplifying solutions.',
+      icon: MessageIcon,
+    },
+    {
+      title: 'Secure Protocols',
+      subtitle: 'A fortress of code guarding your data.',
+      icon: MessageIcon,
+    },
+    {
+      title: 'User Anonymity',
+      subtitle: "Your identity cloaked in the network's shadows.",
+      icon: MessageIcon,
+    },
+  ];
+
+  const featuredAccordionSections = [
+    {
+      number: 1,
+      title: 'Encryption Keys',
+      description: 'Generate unbreakable keys, secure your code.',
+    },
+    {
+      number: 2,
+      title: 'System Penetration',
+      description: 'Test defenses, exploit vulnerabilities effectively.',
+    },
+    {
+      number: 3,
+      title: 'Data Extraction',
+      description: 'Retrieve critical intel with precision operations.',
+    },
+  ];
+
   return (
     <div className='w-full h-[4106px] bg-white flex-col justify-start items-center inline-flex'>
       <NavBar6 />
@@ -24,9 +62,27 @@ export default function Home() {
         heading='Crafting the Cyber Narrative'
         text='Innovation through every byte.'
       />
-      <MobilePreviewSection6 />
-      <NumberAccordion6 />
-      <Newsletter6 />
+      <MobilePreviewSection
+        title='Stealth Operations'
+        subtitle='Navigate with unseen agility.'
+        mobileImage={MobileMockupImage}
+        bullets={featuredBullets}
+      />
+      <NumberAccordion
+        title='Commence the Protocol'
+        accordionSections={featuredAccordionSections}
+      />
+
+      <Newsletter
+        title='Enlist Now'
+        subtitle='Join the Shadow Network!'
+        buttonOptionOneText='Proceed with OAuth'
+        inputLabel='Username or Email'
+        input='Enter Access Credentials'
+        buttonOptionTwoText='Continue with Secrecy'
+        legalText='Consent to our Cryptic Terms and Covert Policy'
+        image={1}
+      />
       <Footer6 />
     </div>
   );

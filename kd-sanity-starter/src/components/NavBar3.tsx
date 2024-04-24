d8077913cd8305c4d6853b035fd9d36f9db33331
Kirsten Bachert <kb@buildmidwestern.com>
2024-04-24 09:27:02 -0600

    fix: condense navbar 3

--- kd-sanity-starter/src/components/NavBar3.tsx
+++ kd-sanity-starter/src/components/NavBar3.tsx
@@ -1,52 +1,93 @@
-import Image from 'next/image';
-import NeonThriveStackIcon from '../../public/NeonThriveStackIcon.svg';
+import Image, { StaticImageData } from 'next/image';
 
-const NavBar3 = () => {
+const NavBar3 = ({
+  icon,
+  pageTitle,
+  linkOneTitle,
+  linkTwoTitle,
+  linkThreeTitle,
+  linkOneTitleB,
+  linkTwoTitleB,
+  linkThreeTitleB,
+  buttonText,
+  isButtonRounded,
+  backgroundColor,
+  textColor,
+  font,
+  buttonBorderColor,
+  isNavBar1 = true,
+  linkSection = true,
+  pageTitleSection = true,
+}: {
+  icon: StaticImageData;
+  pageTitle?: string;
+  linkOneTitle?: string;
+  linkTwoTitle?: string;
+  linkThreeTitle?: string;
+  linkOneTitleB?: string;
+  linkTwoTitleB?: string;
+  linkThreeTitleB?: string;
+  buttonText?: string;
+  isButtonRounded?: boolean;
+  backgroundColor?: string;
+  textColor?: string;
+  font?: string;
+  buttonBorderColor?: string;
+  isNavBar1?: boolean;
+  linkSection?: boolean;
+  pageTitleSection?: boolean;
+}) => {
   return (
     <div className='z-[2] w-full bg-black h-[67px] p-2 justify-between items-center inline-flex'>
       <div className='grow shrink basis-0 h-[51px] px-2 py-4 bg-gray-100 rounded-[64px] justify-between items-center flex'>
-        <div className='px-4 justify-center items-center gap-4 flex'>
-          <div className='w-[50px] self-stretch px-2 justify-start items-center gap-2 flex'>
-            <div className="text-zinc-900 text-xs font-normal font-['Open Sans'] leading-tight">
-              Portal
+        {linkSection && (
+          <div className='px-4 justify-center items-center gap-4 flex'>
+            <div className='w-[50px] self-stretch px-2 justify-start items-center gap-2 flex'>
+              <div className="text-zinc-900 text-xs font-normal font-['Open Sans'] leading-tight">
+                {linkOneTitle}
+              </div>
+            </div>
+            <div className='w-[65px] self-stretch px-2 justify-start items-center gap-2 flex'>
+              <div className="text-zinc-900 text-xs font-normal font-['Open Sans'] leading-tight">
+                {linkTwoTitle}
+              </div>
+            </div>
+            <div className='w-[45px] self-stretch px-2 justify-start items-center gap-2 flex'>
+              <div className="text-zinc-900 text-xs font-normal font-['Open Sans'] leading-tight">
+                {linkThreeTitle}
+              </div>
             </div>
           </div>
-          <div className='w-[65px] self-stretch px-2 justify-start items-center gap-2 flex'>
-            <div className="text-zinc-900 text-xs font-normal font-['Open Sans'] leading-tight">
-              Dystopia
+        )}
+        {pageTitleSection && (
+          <div className='justify-center items-center gap-2 flex'>
+            <div className='w-6 h-6 relative'>
+              <Image alt='' src={icon} />
+            </div>
+            <div className="text-zinc-900 text-base font-normal font-['Open Sans'] leading-normal">
+              {pageTitle}
             </div>
           </div>
-          <div className='w-[45px] self-stretch px-2 justify-start items-center gap-2 flex'>
-            <div className="text-zinc-900 text-xs font-normal font-['Open Sans'] leading-tight">
-              Crew
+        )}
+        {linkSection && (
+          <div className='px-4 justify-center items-center gap-4 flex'>
+            <div className='w-[72px] self-stretch px-2 justify-start items-center gap-2 flex'>
+              <div className="text-zinc-900 text-xs font-normal font-['Open Sans'] leading-tight">
+                {linkOneTitleB}
+              </div>
+            </div>
+            <div className='w-[88px] self-stretch px-2 justify-start items-center gap-2 flex'>
+              <div className="text-zinc-900 text-xs font-normal font-['Open Sans'] leading-tight">
+                {linkTwoTitleB}
+              </div>
+            </div>
+            <div className='w-[63px] self-stretch px-2 justify-start items-center gap-2 flex'>
+              <div className="text-zinc-900 text-xs font-normal font-['Open Sans'] leading-tight">
+                {linkThreeTitleB}
+              </div>
             </div>
           </div>
-        </div>
-        <div className='justify-center items-center gap-2 flex'>
-          <div className='w-6 h-6 relative'>
-            <Image alt='' src={NeonThriveStackIcon} />
-          </div>
-          <div className="text-zinc-900 text-base font-normal font-['Open Sans'] leading-normal">
-            Neon Thrive
-          </div>
-        </div>
-        <div className='px-4 justify-center items-center gap-4 flex'>
-          <div className='w-[72px] self-stretch px-2 justify-start items-center gap-2 flex'>
-            <div className="text-zinc-900 text-xs font-normal font-['Open Sans'] leading-tight">
-              Manifesto
-            </div>
-          </div>
-          <div className='w-[88px] self-stretch px-2 justify-start items-center gap-2 flex'>
-            <div className="text-zinc-900 text-xs font-normal font-['Open Sans'] leading-tight">
-              @cyberpulse
-            </div>
-          </div>
-          <div className='w-[63px] self-stretch px-2 justify-start items-center gap-2 flex'>
-            <div className="text-zinc-900 text-xs font-normal font-['Open Sans'] leading-tight">
-              Connect
-            </div>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
--- kd-sanity-starter/src/components/NavBarNew.tsx
+++ kd-sanity-starter/src/components/NavBarNew.tsx
@@ -13,6 +13,9 @@ export const NavBarNew = ({
   font,
   buttonBorderColor,
   isNavBar1 = true,
+  pageTitleSection = true,
+  linkSection = true,
+  buttonSection = true,
 }: {
   icon: StaticImageData;
   pageTitle?: string;
@@ -26,53 +29,64 @@ export const NavBarNew = ({
   font?: string;
   buttonBorderColor?: string;
   isNavBar1?: boolean;
+  pageTitleSection?: boolean;
+  linkSection?: boolean;
+  buttonSection?: boolean;
 }) => {
   return (
     <div
       className={`z-[2] self-stretch h-[59px] p-2 ${backgroundColor} justify-between items-center inline-flex`}
     >
-      <div className='px-2 justify-start items-center gap-2 flex'>
-        <div className={isNavBar1 ? 'w-6 h-6 relative' : ''}>
-          <Image src={icon} alt='' />
-        </div>
-        <div
-          className={`${textColor} ${isNavBar1 ? 'text-base' : 'text-sm'} font-normal ${font} ${isNavBar1 ? 'leading-normal' : 'leading-[21px]'}`}
-        >
-          {pageTitle}
-        </div>
-      </div>
-      <div className={`w-${isNavBar1 ? '334' : '307'} px-4 justify-start items-center gap-4 flex`}>
-        <div
-          className={`w-${isNavBar1 ? '100' : '62'} self-stretch px-4 justify-start items-center gap-2 flex`}
-        >
-          <div className={`${textColor} text-xs font-normal ${font} leading-tight`}>
-            {linkOneTitle}
+      {pageTitleSection && (
+        <div className='px-2 justify-start items-center gap-2 flex'>
+          <div className={isNavBar1 ? 'w-6 h-6 relative' : ''}>
+            <Image src={icon} alt='' />
+          </div>
+          <div
+            className={`${textColor} ${isNavBar1 ? 'text-base' : 'text-sm'} font-normal ${font} ${isNavBar1 ? 'leading-normal' : 'leading-[21px]'}`}
+          >
+            {pageTitle}
           </div>
         </div>
+      )}
+      {linkSection && (
         <div
-          className={`w-${isNavBar1 ? '100' : '70'} self-stretch px-4 justify-start items-center gap-2 flex`}
+          className={`w-${isNavBar1 ? '334' : '307'} px-4 justify-start items-center gap-4 flex`}
         >
-          <div className={`${textColor} text-xs font-normal ${font} leading-tight`}>
-            {linkTwoTitle}
+          <div
+            className={`w-${isNavBar1 ? '100' : '62'} self-stretch px-4 justify-start items-center gap-2 flex`}
+          >
+            <div className={`${textColor} text-xs font-normal ${font} leading-tight`}>
+              {linkOneTitle}
+            </div>
+          </div>
+          <div
+            className={`w-${isNavBar1 ? '100' : '70'} self-stretch px-4 justify-start items-center gap-2 flex`}
+          >
+            <div className={`${textColor} text-xs font-normal ${font} leading-tight`}>
+              {linkTwoTitle}
+            </div>
+          </div>
+          <div
+            className={`w-${isNavBar1 ? '100' : '111'} self-stretch px-4 justify-start items-center gap-2 flex`}
+          >
+            <div className={`${textColor} text-xs font-normal ${font} leading-tight`}>
+              {linkThreeTitle}
+            </div>
           </div>
         </div>
+      )}
+      {buttonSection && (
         <div
-          className={`w-${isNavBar1 ? '100' : '111'} self-stretch px-4 justify-start items-center gap-2 flex`}
+          className={`w-${isNavBar1 ? '121' : '147'} px-5 py-3 ${isButtonRounded ? 'rounded-lg' : ''} border ${buttonBorderColor} justify-center items-center gap-1 flex`}
         >
-          <div className={`${textColor} text-xs font-normal ${font} leading-tight`}>
-            {linkThreeTitle}
+          <div className='px-1 justify-center items-center gap-2 flex'>
+            <div className={`text-center ${textColor} text-xs font-normal ${font} leading-tight`}>
+              {buttonText}
+            </div>
           </div>
         </div>
-      </div>
-      <div
-        className={`w-${isNavBar1 ? '121' : '147'} px-5 py-3 ${isButtonRounded ? 'rounded-lg' : ''} border ${buttonBorderColor} justify-center items-center gap-1 flex`}
-      >
-        <div className='px-1 justify-center items-center gap-2 flex'>
-          <div className={`text-center ${textColor} text-xs font-normal ${font} leading-tight`}>
-            {buttonText}
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
